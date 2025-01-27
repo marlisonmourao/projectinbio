73dc707d7b39e8cadb9844dfe7eb055ca289c9c2
marlisonmourao <bentesmourao@gmail.com>
2025-01-26 21:59:01 -0400

    feat: header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Header } from '@/components/lading-page/header'
 import Hero from '@/components/lading-page/hero'
-
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
-      {/* <Header />
+      <Header />
+      {/*
       <VideoExplanation />
       <Pricing />
       <FAQ /> */}
