664aa95ab2c510696e8747e5cb19e5b75c2c2816
marlisonmourao <bentesmourao@gmail.com>
2025-01-27 21:44:17 -0400

    feat: add pricing and video explanation sections to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import { Header } from '@/components/lading-page/header'
 import Hero from '@/components/lading-page/hero'
+import { Pricing } from '@/components/lading-page/pricing'
+import { VideoExplanation } from '@/components/lading-page/video-explanation'
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       {/*
-      <VideoExplanation />
-      <Pricing />
+      
       <FAQ /> */}
 
       <Hero />
+      <VideoExplanation />
+      <Pricing />
     </div>
   )
 }
